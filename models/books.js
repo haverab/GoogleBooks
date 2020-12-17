c5dcebb7aeb608c5059c53a175047b23d7b5305e
Allison Haver <allisonbhaver@gmail.com>
2020-12-16 23:44:15 -0500

    added index.js file in models folder

--- models/books.js
+++ models/books.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
   title: { type: [String], required: true },
   author: { type: [String], required: true },
-  description: String,
+  description: {type: [String]},
 });
 
 const Book = mongoose.model("Book", bookSchema);
